dc5d7964ffd5be444211192ccc937158084483a1
Nadezhda <el.shtolts@yandex.ru>
2024-10-01 19:10:32 +0300

    created base UI elements

--- TicTacToeApp/TicTacToeApp/Onboarding Screen/OnboardingVC.swift
+++ TicTacToeApp/TicTacToeApp/Onboarding Screen/OnboardingVC.swift
@@ -12,17 +12,31 @@ class OnboardingVC: UIViewController {
     
     let xoImage = UIImageView(image: UIImage(named: "XO"))
     
-    private let nameGame: UILabel = {
+    private let settingButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "Setting"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private let rulesButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "Question"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let nameGameLabel: UILabel = {
         let label = UILabel()
         label.text = "TIC-TAC-TOE"
         label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 28.0, weight: .medium)
-        label.textColor = UIColor(red: 137/255, green: 68/255, blue: 171/255, alpha: 1.0)
+        label.font = .SFProDisplay.bold.size(of: 32)
+        label.textColor = .black
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     let playButton = UIButton.createButton(title:"Let's play",
+                                           foregroundColor: .white, 
                                            backgroundColor: AppColors.basicBlue)
    
     
@@ -33,21 +47,40 @@ class OnboardingVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(playButton)
+        view.addSubview(settingButton)
+        view.addSubview(rulesButton)
         view.addSubview(xoImage)
+        view.addSubview(nameGameLabel)
+        view.addSubview(playButton)
+        
         setupView()
     }
     
     
     
     func setupView() {
-        xoImage.translatesAutoresizingMaskIntoConstraints = false
+        
         playButton.translatesAutoresizingMaskIntoConstraints = false
+        xoImage.translatesAutoresizingMaskIntoConstraints = false
+        settingButton.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
+            
+            settingButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 25),
+            settingButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            rulesButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 25),
+            rulesButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
             xoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            xoImage.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-
+            xoImage.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100),
+            
+            nameGameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nameGameLabel.topAnchor.constraint(equalTo: xoImage.bottomAnchor, constant: 20),
+            
+            playButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -45),
+            playButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            playButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15)
 
         ])
     }
