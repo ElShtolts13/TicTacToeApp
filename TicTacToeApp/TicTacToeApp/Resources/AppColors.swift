3b0d03405b163f2a64293a3f31415e208a46f792
Suharik001 <suharev-a@list.ru>
2024-10-01 19:01:24 +0300

    Added HowToPlayVC

--- TicTacToeApp/TicTacToeApp/Resources/AppColors.swift
+++ TicTacToeApp/TicTacToeApp/Resources/AppColors.swift
@@ -10,8 +10,8 @@ import UIKit
 enum AppColors {
     static let background = UIColor().hex(0xF5F7FF)
     static let basicBlue = UIColor ().hex(0x8480D4)
+    static let basicBlack = UIColor().hex(0x232946)
     static let basicLightBlue = UIColor().hex(0xE6E9F9)
     static let secondaryPurple = UIColor().hex(0xD5C1F6)
-
 }
 
---
+++ TicTacToeApp/TicTacToeApp/Resources/ConstantAppString.swift
@@ -0,0 +1,15 @@
+//
+//  ConstantAppString.swift
+//  TicTacToeApp
+//
+//  Created by Сергей Сухарев on 01.10.2024.
+//
+
+import UIKit
+
+enum ConstantAppString {
+    static let text1 = "Draw a grid with three rows and three columns, creating nine squares in total."
+    static let text2 = "Players take turns placing their marker (X or O) in an empty square.\nTo make a move, a player selects a number corresponding to the square where they want to place their marker."
+    static let text3 = "Player X starts by choosing a square (e.g., square 5).\nPlayer O follows by choosing an empty square (e.g., square 1).\nContinue alternating turns until the game ends."
+    static let text4 = "The first player to align three of their markers horizontally, vertically, or diagonally wins.\nExamples of Winning Combinations:\nHorizontal: Squares 1, 2, 3 or 4, 5, 6 or 7, 8, 9\nVertical: Squares 1, 4, 7 or 2, 5, 8 or 3, 6, 9\nDiagonal: Squares 1, 5, 9 or 3, 5, 7"
+}
